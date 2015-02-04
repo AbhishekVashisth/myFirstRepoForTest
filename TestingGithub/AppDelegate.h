911b5855843284736e275759dc754fc77b4bb410
apple <apple@apples-iMac-2.local>
2015-02-04 12:05:10 +0530

    Initial Commit

---
+++ TestingGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingGithub
+//
+//  Created by apple on 04/02/15.
+//  Copyright (c) 2015 PSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingGithub
+//
+//  Created by apple on 04/02/15.
+//  Copyright (c) 2015 PSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
